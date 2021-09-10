13c8908a316e51089c560382c9e70e282435cc94
Your Name <liza.volk999@gmail.com>
2021-09-10 13:43:21 +0300

    Initial commit

---
+++ app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
@@ -0,0 +1,11 @@
+package quizapp.volkova.diceroller_codelab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
