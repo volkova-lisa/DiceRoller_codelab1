eb1147b57b5c66059ec01db987dd11eac5c39017
Your Name <liza.volk999@gmail.com>
2021-09-10 13:51:56 +0300

    toast added

--- app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
@@ -2,10 +2,20 @@ package quizapp.volkova.diceroller_codelab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDiceToast()}
+
+    }
+
+    private fun rollDiceToast() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
