20b159b9feeab00d07c6951e43a18a8e64a6e7cc
Your Name <liza.volk999@gmail.com>
2021-09-10 14:31:20 +0300

    Coding Challenge

--- app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
@@ -7,22 +7,30 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var rollButton: Button
-    lateinit var textView: TextView
+
     private val randomNum: Int = (1..6).random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rollButton = findViewById(R.id.button)
-        textView = findViewById(R.id.textView)
+        val rollButton: Button = findViewById(R.id.button)
+        val countUpButton: Button = findViewById(R.id.button2)
+        val textView: TextView = findViewById(R.id.textView)
 
         rollButton.setOnClickListener { rollDiceToast(textView)}
-
+        countUpButton.setOnClickListener{ countUp(textView) }
     }
 
     private fun rollDiceToast(textV : TextView) {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         textV.text = randomNum.toString()
     }
+
+    private fun countUp(textV : TextView){
+        val value1 = textV.text.toString()
+        val valueToNum = value1.toInt()
+        val counted = valueToNum + 1
+        textV.text = counted.toString()
+    }
+
 }
