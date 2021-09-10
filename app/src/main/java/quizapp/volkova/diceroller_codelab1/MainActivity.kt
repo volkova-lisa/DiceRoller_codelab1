b1d8ad1fd76d0910f8a6f353cf440cf7968e43b1
Your Name <liza.volk999@gmail.com>
2021-09-10 13:56:58 +0300

    button works

--- app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/diceroller_codelab1/MainActivity.kt
@@ -3,19 +3,23 @@ package quizapp.volkova.diceroller_codelab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val rollButton: Button = findViewById(R.id.button)
+    private val textView: TextView = findViewById(R.id.textView)
+    private val randomNum: Int = (1..6).random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDiceToast()}
 
     }
 
     private fun rollDiceToast() {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        textView.text = randomNum.toString()
     }
 }
